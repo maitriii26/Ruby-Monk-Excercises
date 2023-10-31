47dcbf6c5fdeb97be309450ef5c2261554e23013
rutvikvyas45 <rutvik.vyas@humive.com>
2023-10-31 19:26:18 +0530

    New commit

---
+++ 10) Introduction_to_Ruby_Hashes.rb
@@ -0,0 +1,27 @@
+{}
+
+student_ages = {
+    "Jack" => 10,
+    "Jill" => 12,
+    "Bob" => 14
+}
+print student_ages
+
+print "\n"
+
+restaurant_menu = {
+    "Tea" => 10,
+    "Coffee" => 20,
+    "Beverages" => 50
+}
+puts restaurant_menu["Tea"]
+puts restaurant_menu["Coffee"]= 30
+print restaurant_menu
+
+print "\n"
+
+menu = {}
+menu["Dal"] = 30
+menu["Roti"] = 10
+menu["Sabji"] = 50
+print menu
